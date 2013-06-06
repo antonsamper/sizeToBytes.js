2572efa416ae5ee15c6320201c1a8e279e55a2db
Anton Samper Rivaya <anton@aeser.co.uk>
2013-06-06 21:41:33 +0100

    changed tabs to spaces

--- sizeToBytes.js
+++ sizeToBytes.js
@@ -1,21 +1,21 @@
 var sizeToBytes = (function () {
 
-	'use strict';
+    'use strict';
 
-	var base = 1024,
-		sizes = {
-			'KB': Math.pow(base, 1),
-			'MB': Math.pow(base, 2),
-			'GB': Math.pow(base, 3),
-			'TB': Math.pow(base, 4),
-			'PB': Math.pow(base, 5),
-			'EB': Math.pow(base, 6),
-			'ZB': Math.pow(base, 7),
-			'YB': Math.pow(base, 8)
-		};
+    var base  = 1024,
+        sizes = {
+            'KB': Math.pow(base, 1),
+            'MB': Math.pow(base, 2),
+            'GB': Math.pow(base, 3),
+            'TB': Math.pow(base, 4),
+            'PB': Math.pow(base, 5),
+            'EB': Math.pow(base, 6),
+            'ZB': Math.pow(base, 7),
+            'YB': Math.pow(base, 8)
+        };
 
-	return function (amount, format) {
-		return amount * sizes[format];
-	};
+    return function (amount, format) {
+        return amount * sizes[format];
+    };
 
 }());
